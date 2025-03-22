e79d0ad78eba79f625fef2c8f4151eb50abe5e8f
vandanabudaraju <vandanabudaraju99@gmail.com>
2025-03-22 13:05:06 +0530

    ping app service

---
+++ server.js
@@ -0,0 +1,12 @@
+import express from "express";
+
+const app = express()
+const port = 3000
+
+app.get('/ping', (req, res) => {
+    res.send('pong')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
